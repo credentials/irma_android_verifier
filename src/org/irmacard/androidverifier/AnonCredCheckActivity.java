3be1387adeffec3defce4da0f2782272387d0595
Maarten Everts <maarten.everts@tno.nl>
2012-10-03 09:14:06 +0200

    Add LICENSE and AUTHORS files.

--- src/org/irmacard/androidverifier/AnonCredCheckActivity.java
+++ src/org/irmacard/androidverifier/AnonCredCheckActivity.java
@@ -1,3 +1,24 @@
+/**
+ * AnonCredCheckActivity.java
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) Maarten Everts, TNO, July 2012,
+ * Copyright (C) Wouter Lueks, Radboud University Nijmegen, July 2012.
+ */
+
+
 package org.irmacard.androidverifier;
 
 import java.util.ArrayList;
@@ -34,6 +55,11 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+/**
+ * Main Activity for the IRMA android verifier app.
+ * @author Maarten Everts, TNO.
+ *
+ */
 public class AnonCredCheckActivity extends Activity {
 	
 	CheckResultAdapter checkresults;
